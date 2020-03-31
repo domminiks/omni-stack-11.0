fb1a9b7ceeb4a1fcef0d599478a111839b369c8c
Pedro Dominguite <dominguite@PedroMacBook.local>
2020-03-31 12:18:20 -0300

    Add frontend

---
+++ frontend/src/pages/NewIncident/index.js
@@ -0,0 +1,74 @@
+import React, {useState} from 'react';
+import {Link, useHistory} from 'react-router-dom';
+import {FiArrowLeft} from 'react-icons/fi';
+
+import api from '../../services/api';
+
+import logoImg from "../../assets/logo.svg"
+
+import './styles.css';
+
+export default function NewIncident() {
+
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [value, setValue] = useState('');
+
+    const ongId = localStorage.getItem('ongId');
+
+    const history = useHistory();
+
+    async function handleNewIncident(e) {
+        e.preventDefault();
+        const data = {
+            title,
+            description,
+            value
+        }
+        try {
+            await api.post('incidents', data, {
+                headers : {
+                    Authorization : ongId
+                }
+            });
+            history.push('/profile');
+        } catch(err) {
+            alert('Error registering a new incident. Try again.');
+        }
+    }
+
+    return (
+        <div className="new-incident-container">
+            <div className="content">
+                <section>
+                    <img src={logoImg} alt="Be The Hero!" />
+                    <h1>Register a new incident</h1>
+                    <p>Describe the incident in detail and find a hero to fight it.</p>
+
+                    <Link to="/" className="back-link">
+                        <FiArrowLeft size={16} color="#E02041" />
+                        Home
+                    </Link>
+                </section>
+                <form onSubmit={handleNewIncident}>
+                    <input 
+                        placeholder="Incident title"
+                        onChange={e => setTitle(e.target.value)}
+                        value={title}
+                    />
+                    <textarea 
+                        placeholder="Description"
+                        value={description}
+                        onChange={e => setDescription(e.target.value)}
+                    />
+                    <input 
+                        placeholder="Value in R$"
+                        value={value}
+                        onChange={e => setValue(e.target.value)}
+                    />
+                    <button className="button" type="submit">Register</button>
+                </form>
+            </div>
+        </div>
+    );
+}
---
+++ frontend/src/pages/Register/index.js
@@ -0,0 +1,89 @@
+import React, { useState } from 'react';
+import {Link, useHistory} from 'react-router-dom';
+import {FiArrowLeft} from 'react-icons/fi';
+
+import api from '../../services/api';
+
+import logoImg from "../../assets/logo.svg"
+
+import './styles.css';
+
+export default function Register() {
+    const[name, setName] = useState('');
+    const[email, setEmail] = useState('');
+    const[whatsapp, setWhatsapp] = useState('');
+    const[city, setCity] = useState('');
+    const[uf, setUf] = useState('');
+
+    const history = useHistory();
+
+    async function handleRegister(e) {
+        //The page will not reload
+        e.preventDefault();
+
+        const data = {
+            name,
+            whatsapp, 
+            email, 
+            city,
+            uf
+        };
+
+        try {
+            const response = await api.post('ongs', data);
+            alert(`Your ID: ${response.data.id}`);
+            history.push('/');
+        }  catch(err) {
+            alert(err);
+            alert("Subscription error. Try again.");
+        }
+    }
+
+    return (
+        <div className="register-container">
+            <div className="content">
+                <section>
+                    <img src={logoImg} alt="Be The Hero!" />
+                    <h1>Subscription</h1>
+                    <p>Finish your subscription, access the platform and let people help with your incidents.</p>
+                    <Link to="/" className="back-link">
+                        <FiArrowLeft size={16} color="#E02041" />
+                        I don't have an ID  
+                    </Link>
+                </section>
+                <form onSubmit={handleRegister}>
+                    <input 
+                        placeholder="ONG name"
+                        value={name}
+                        onChange={e => setName(e.target.value)}
+                    />
+                    <input 
+                        type="email" 
+                        placeholder="E-Mail"
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
+                    />
+                    <input 
+                        placeholder="WhatsApp"
+                        value={whatsapp}
+                        onChange={e => setWhatsapp(e.target.value)}    
+                    />
+                    <div className="input-group">
+                        <input 
+                            placeholder="City"
+                            value={city}
+                            onChange={e => setCity(e.target.value)}
+                        />
+                        <input 
+                            placeholder="State" 
+                            style={{width:90}}
+                            value={uf}
+                            onChange={e => setUf(e.target.value)}
+                        />
+                    </div>
+                    <button className="button" type="submit">Register</button>
+                </form>
+            </div>
+        </div>
+    );
+}
